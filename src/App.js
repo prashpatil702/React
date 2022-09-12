b5ec911734d5a0650a0d1ef909593a9d7de51ae5
prashpatil702 <prashpatil702@gmail.com>
2022-09-12 23:00:55 +0530

    added counter logic

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Greet from './components/Greet'
 import Welcome from './components/Welcome'
 import Message from './components/Message';
+import Counter from './components/Counter';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
       <Welcome name="Third" heroname="Spiderman" />
 
       <Message></Message>
+      <Counter></Counter>
     </div>
   );
 }
